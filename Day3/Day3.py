34d76495f48990facdb55e49c9e52889e8c5046e
Bad_Jubies <gthume125@gmail.com>
2020-12-03 08:55:59 -0600

    python - day 3

---
+++ Day3/Day3.py
@@ -0,0 +1,28 @@
+import math
+
+def moveToboggan(right,down):
+    file = open("input.txt", "r").read().splitlines()
+    d = 0
+    c = 0
+    r = 0
+
+    while d < len(file):
+        if (file[d]*100000)[r] == '#':
+            c+=1
+        r += right
+        d += down
+    return c
+
+def main():
+    one = moveToboggan(1,1)
+    two = moveToboggan(3,1)
+    three = moveToboggan(5,1)
+    four = moveToboggan(7,1)
+    five = moveToboggan(1,2)
+
+    answer = one * two * three * four * five
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
