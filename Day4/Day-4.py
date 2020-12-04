91829629e13a776c3e7084989b558b63bde494bf
Bad_Jubies <gthume125@gmail.com>
2020-12-04 13:56:18 -0600

    progress

--- Day4/Day-4.py
+++ Day4/Day-4.py
@@ -1,6 +1,10 @@
-batfile = open("input.txt","r").read().split("\n\n")
+import re
+import itertools
 
-filters = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+openfile = open("input.txt", "r")
+batfile = openfile.read().split("\n\n")
+
+filters = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 filtered_list = []
 
@@ -8,9 +12,25 @@ for p in batfile:
     if all((f in p) for f in filters):
         filtered_list += [p]
 
+c = len(filtered_list)
+
+for i in filtered_list:
+    try:
+        forms = re.split('[\n ]', i)
+        for form in forms:
+            d = form.split(":")
+            if "iyr" in d[0] and 2010 <= int(d[1]) <= 2020:
+                print("yes")
+    except:
+
+openfile.close()
+
 """
 byr: 1920 => 2002
 iyr: 2010 => 2020
 eyr: 2020 => 2030
-hgt: digit followed by 
+hgt: number followed by 'cm' or 'in'
+hcl: a '#' followed by exactly sic characters 0-9 or a-f
+ecl: exactly one of: amb, blu, brn, gry, grn, hzl, oth
+pid: a nine-digit number, including leading zeroes
 """
