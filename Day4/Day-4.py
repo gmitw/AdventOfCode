86ec34a4421c41fc972b583e91030d8cf3b904cd
Bad_Jubies <gthume125@gmail.com>
2020-12-04 11:01:22 -0600

    REMOVE SPACE

--- Day4/Day-4.py
+++ Day4/Day-4.py
@@ -2,10 +2,15 @@ batfile = open("input.txt","r").read().split("\n\n")
 
 filters = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
 
-c = 0
+filtered_list = []
 
 for p in batfile:
     if all((f in p) for f in filters):
-        c += 1
+        filtered_list += [p]
 
-print(c)
+"""
+byr: 1920 => 2002
+iyr: 2010 => 2020
+eyr: 2020 => 2030
+hgt: digit followed by 
+"""
