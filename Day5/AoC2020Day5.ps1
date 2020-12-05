e29f225788dfa6e77fce04c545ccbbe5e835aee0
gmitw <75438257+gmitw@users.noreply.github.com>
2020-12-05 02:44:29 -0600

    Added grid of passengers with my seat highlighted

--- Day5/AoC2020Day5.ps1
+++ Day5/AoC2020Day5.ps1
@@ -39,9 +39,52 @@ $locations = $boardingPasses | ForEach-Object { Get-SeatLocation -BoardingPass $
 $maxSeatID = ($locations | Sort-Object SeatId | Select-Object -Last 1).SeatID
 $minSeatID = ($locations | Sort-Object SeatId | Select-Object -First 1).SeatID
 
+$myCol=-1
+$myRow=-1
+$mySeatId=-1
 Write-Host "Highest SeatId is $maxSeatId"
 foreach ($seatID in $minSeatID..$maxSeatID) {
     if ($locations.SeatID -notcontains $seatID -and $locations.SeatId -contains ($seatID-1) -and $locations.SeatId -contains ($seatID+1)) {
         Write-Host "My SeatId is $seatId"
+        $myRow = ($locations | Where-Object SeatId -eq ($seatID-1)).Row
+        $myCol = ($locations | Where-Object SeatId -eq ($seatID-1)).Col+1
+        if ($myCol -gt 7) {
+            $myCol=0
+            $myRow++
+        }
+        $mySeatId = $seatID
     }
+}
+
+
+
+Write-Host "     |  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |"
+Write-Host "-----+-----+-----+-----+-----+-----+-----+-----+-----+"
+
+foreach ($row in (0..127)) {
+    $numSpaces = 3 - ([String]$row).Length
+    foreach ($space in 0..$numSpaces) {
+        Write-Host " " -NoNewline
+    }
+    Write-Host "$row |" -NoNewline
+
+    foreach ($col in 0..7) {
+        $currentSeatID = ($locations | Where-Object Col -eq $col | Where-Object Row -eq $row).SeatId
+        $magenta=$false
+        if ($row -eq $myRow -and $col -eq $myCol) {
+            $currentSeatID = $mySeatId
+            $magenta=$true
+        }
+        $numSpaces = 3 - ([String]$currentSeatID).Length
+        foreach ($space in 0..$numSpaces) {
+            Write-Host " " -NoNewline
+        }
+        if ($magenta) {
+            Write-Host "$currentSeatID " -NoNewline -ForegroundColor Magenta
+            Write-Host "|" -NoNewline
+        } else {
+            Write-Host "$currentSeatID |" -NoNewline
+        }
+    }
+    Write-Host ""
 }
